49b3f9e5657e8c08353db11954375d0891837aed
pravallikakaranam <pravallika4748@gmail.com>
2019-05-13 21:12:37 +0530

    topic service is added in course-api code

--- course-api/src/main/java/io/javabrains/springbootstarter/topics/TopicsController.java
+++ course-api/src/main/java/io/javabrains/springbootstarter/topics/TopicsController.java
@@ -3,17 +3,18 @@ package io.javabrains.springbootstarter.topics;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicsController {
+	
+	@Autowired
+	private TopicsService topicService;
 
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-				new Topic(1,"Java","we are using java 1.8 version."),
-				new Topic(2,"Spring Boot","we are using spring boot 1.4.2 version.")
-				);
+		return topicService.getAllTopics();
 	}
 }
